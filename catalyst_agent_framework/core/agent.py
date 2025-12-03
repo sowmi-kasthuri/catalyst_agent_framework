5c741b97c1f0630c45a86b9773a60449f54f3ebc
sowmi-kasthuri <sowmi.kasthuri@gmail.com>
2025-12-03 22:01:18 +0530

    Day 3 - Added user, assistant, system helper methods

--- catalyst_agent_framework/core/agent.py
+++ catalyst_agent_framework/core/agent.py
@@ -10,3 +10,12 @@ class BaseAgent(ABC):
 
     def add_message(self, role:str, content:str):
         self.history.append({"role":role,"content":content})
+    
+    def user(self, content:str):
+        self.add_message("user",content)
+    
+    def assistant(self, content:str):
+        self.add_message("assistant",content)
+    
+    def system(self,content:str):
+        self.add_message("system",content)
