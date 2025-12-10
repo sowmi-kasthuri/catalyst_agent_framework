189517da200041352b09c0541d390365e117a5b2
sowmi-kasthuri <sowmi.kasthuri@gmail.com>
2025-12-10 11:39:23 +0530

    Day 10 - Added OpenAIEngine skeleton

---
+++ catalyst_agent_framework/engines/openai_engine.py
@@ -0,0 +1,13 @@
+from ..core.llm_engine import LLMEngine
+
+class OpenAIEngine(LLMEngine):
+    def __init__(self, client, model: str, **defaults):
+        self.client = client
+        self.model = model
+        self.defaults = defaults
+    
+    def generate(self, prompt: str, **kwargs):
+        pass
+
+    def call_tools(self, prompt: str, tools: list, **kwargs):
+        pass
