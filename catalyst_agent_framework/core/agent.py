de74bba85913ac12842b36054aa22621cf762642
sowmi-kasthuri <sowmi.kasthuri@gmail.com>
2025-12-15 22:00:19 +0530

    Day 15 - Wired logger, metrics, and start event into BaseAgent

--- catalyst_agent_framework/core/agent.py
+++ catalyst_agent_framework/core/agent.py
@@ -1,10 +1,21 @@
 from abc import ABC, abstractmethod
 from .response import Message
 
+from ..utils.logger import Logger
+from ..utils.metrics import Metrics
+from .events import Event
+
 class BaseAgent(ABC):
     def __init__(self, name: str):
         self.name = name
         self.history: list[Message] = []
+        self.logger = Logger()
+        self.metrics = Metrics()
+    
+    def _emit_start_event(self):
+        event = Event(name="agent.start", payload={"agent": self.name})
+        self.logger.log("INFO", "agent.start", event=event.as_dict())
+        self.metrics.increment("agent_start")
     
     @abstractmethod
     def run(self, input_text: str):
